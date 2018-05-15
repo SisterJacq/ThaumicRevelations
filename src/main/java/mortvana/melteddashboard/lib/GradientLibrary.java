7d0d4936b5e3db7c844c9a1f4205a99d55a58588
Mortvana <nyehcraft@gmail.com>
2018-05-15 12:26:58 -0700

    So mortvana.melteddashbord.lib.* stuff was being excluded due to my .gitignore, so I fixed it.

---
+++ src/main/java/mortvana/melteddashboard/lib/GradientLibrary.java
@@ -0,0 +1,10 @@
+package mortvana.melteddashboard.lib;
+
+import mortvana.melteddashboard.client.texture.GradientNode;
+
+public final class GradientLibrary {
+
+	public static final GradientNode[] GRAD_CU = { new GradientNode(0xC95926, 0.0f), new GradientNode(0x4F2B01, 1.0f)};
+
+
+}
